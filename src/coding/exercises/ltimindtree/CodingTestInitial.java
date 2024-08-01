9fc0e05709c6a9c7ef77b4653ab509121fadb54a
SUNAYANAMALLICK <sunayana.mallick@outlook.com>
2024-08-01 16:30:01 +0100

    Daily update

---
+++ src/coding/exercises/ltimindtree/CodingTestInitial.java
@@ -0,0 +1,53 @@
+package coding.exercises.ltimindtree;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class CodingTestInitial {
+    public static void main(String[] args) {
+
+      String s = "aabbbcccc";
+      //a2c3b3
+
+        System.out.println(s);
+
+
+        List<Character> list = s.chars().mapToObj(ch->(char)ch).collect(Collectors.toList());
+
+        StringBuilder reverse = new StringBuilder();
+         for(int i=s.length()-1 ; i>=0;i--){
+             reverse.append(list.get(i));
+         }
+
+        System.out.println(reverse);
+
+
+        Map<Character, Long> map = s.chars().mapToObj(ch->(char)ch).collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
+
+
+        System.out.println(map);
+
+        StringBuilder output = new StringBuilder();
+
+        map.entrySet().stream().forEach(value->{
+
+            output.append(value.getKey());
+            output.append(value.getValue());
+
+        });
+
+        map.put('a', 90L);
+
+        System.out.println(map);
+
+
+        System.out.println(output);
+    }
+}
+
+
+// Payment -> option1, option2 ...
+
+//
---
+++ src/coding/exercises/ltimindtree/CodingTestTechnical.java
@@ -0,0 +1,8 @@
+package coding.exercises.ltimindtree;
+
+public class CodingTestTechnical {
+ public static void main(String[] args) {
+
+
+    }
+}
